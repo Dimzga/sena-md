822f3fa304feae0aafdbef19a9f08ae114bf3c1e
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-11-09 19:33:31 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,15 +1,28 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = await instagramdl(args[0])
-        .catch(async _ => await instagramdlv2(args[0]))
-        .catch(async _ => await instagramdlv3(args[0]))
-        .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
-}
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+import { randomBytes } from 'crypto'
 
-handler.command = /^(ig(dl)?)$/i
+let handler = async (m, { conn, text }) => {
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? `${htki} *BROADCAST* ${htka}\n` + teks : `${htki} *BROADCAST* ${htka}\n` + teks + '\n' + readMore + '\n\n' + botdate), true).catch(_ => _)
+  m.reply('Selesai Broadcast All Group :)')
+  }
+  let vn = `./mp3/insecure.opus`
+  conn.sendFile(m.chat, vn, 'Fangz.Ganz', null, m, true, {
+type: 'audioMessage',
+ptt: true })
+}
+
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(mt)$/i
+
+handler.owner = true
 
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
