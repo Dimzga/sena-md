ce14dd3a978e2dc968dfc3a4cf93d449179cb940
Dimzga <106530321+Dimzga@users.noreply.github.com>
2023-01-22 11:15:03 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-  await conn.sendFile(m.chat, pickRandom(asupan), 'asupan.mp4', `*${htki} BY ELAINA ${htka}*` , m)
+  await conn.sendFile(m.chat, pickRandom(asupan), 'asupan.mp4', `*${htki} BY Sena bot${htka}*` , m)
 }
 handler.help = ['asupan']
 handler.tags = ['tools']
